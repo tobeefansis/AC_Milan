4fed51ccf0593e7e94feee75a2d3cd600a2c12e3
tobeefansis <40244173+tobeefansis@users.noreply.github.com>
2020-02-18 17:13:39 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,10 @@
 // ![](https://flutter.github.io/assets-for-api-docs/assets/widgets/form.png)
 
 import 'package:flutter/material.dart';
-import 'Action.dart';
+import 'MatchList.dart';
 
 void main() => runApp(MyApp());
+
 
 /// This Widget is the main application widget.
 class MyApp extends StatelessWidget {
@@ -19,75 +20,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: _title,
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(title: const Text(_title)),
-        body: MyStatefulWidget(),
-      ),
-    );
-  }
-}
-
-class MyStatefulWidget extends StatefulWidget {
-  MyStatefulWidget({Key key}) : super(key: key);
-
-  @override
-  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
-}
-
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  final _formKey = GlobalKey<FormState>();
-
-  String passStr;
-
-  String loginStr;
-  @override
-  Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Padding(
-            padding:
-                const EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
-            child: TextFormField(
-              onChanged: (text) {
-                loginStr = text;
-              },
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Логин',
-              ),
-            ),
-          ),
-          Padding(
-            padding:
-                const EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
-            child: TextField(
-              onChanged: (text) {
-                passStr = text;
-              },
-              obscureText: true,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Пароль',
-              ),
-            ),
-          ),
-          Padding(
-            padding:
-                const EdgeInsets.symmetric(vertical: 16.0, horizontal: 10.0),
-            child: RaisedButton(
-              onPressed: () {
-                loginAsync(loginStr, passStr);
-
-                final snackBar = SnackBar(content: Text('Вход прошел успешно'), backgroundColor: Color.fromARGB(255, 61, 194, 54));
-                Scaffold.of(context).showSnackBar(snackBar);
-              },
-              child: Text('Вход'),
-            ),
-          ),
-        ],
+        body: ListView(
+          children: MatchList().build(context),
+        ),
       ),
     );
   }
