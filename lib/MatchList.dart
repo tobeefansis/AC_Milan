1e37c180d8528844bdc36956d7a492b3d02bee78
tobeefansis <40244173+tobeefansis@users.noreply.github.com>
2020-02-18 17:13:45 +0300

    Create MatchList.dart

---
+++ lib/MatchList.dart
@@ -0,0 +1,343 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'Match.dart';
+
+class MatchList {
+  List<Match> matchs = [
+    Match(
+        status: "Fixture",
+        players: [
+          Player(name: "player1", voites: 11),
+          Player(name: "player2", voites: 1),
+          Player(name: "player3", voites: 12),
+          Player(name: "player4", voites: 1),
+        ],
+        name: "Milan - SPAL",
+        dateBeginning: "11.14.2019(16:49)",
+        dateEnd: "11.14.2019(16:49)"),
+    Match(
+        status: "Fixture",
+        players: [
+          Player(name: "player1", voites: 11),
+          Player(name: "player2", voites: 1),
+          Player(name: "player3", voites: 12),
+          Player(name: "player4", voites: 1),
+        ],
+        name: "Milan - Napoli",
+        dateBeginning: "11/14/2019 - 16:49",
+        dateEnd: "11/22/2019 - 16:49"),
+    Match(
+        status: "Fixture",
+        players: [
+          Player(name: "player1", voites: 11),
+          Player(name: "player2", voites: 1),
+          Player(name: "player3", voites: 12),
+          Player(name: "player4", voites: 1),
+        ],
+        name: "Juventus - Milan",
+        dateBeginning: "11/14/2019 - 16:49",
+        dateEnd: "11/22/2019 - 16:49"),
+    Match(
+        status: "Fixture",
+        players: [
+          Player(name: "player1", voites: 11),
+          Player(name: "player2", voites: 1),
+          Player(name: "player3", voites: 12),
+          Player(name: "player4", voites: 1),
+        ],
+        name: "Bologna - Milan",
+        dateBeginning: "11/14/2019 - 16:49",
+        dateEnd: "11/22/2019 - 16:49"),
+    Match(
+        status: "Fixture",
+        players: [
+          Player(name: "player1", voites: 11),
+          Player(name: "player2", voites: 1),
+          Player(name: "player3", voites: 12),
+          Player(name: "player4", voites: 1),
+        ],
+        name: "Milan - SPAL",
+        dateBeginning: "11/14/2019 - 16:49",
+        dateEnd: "11/22/2019 - 16:49"),
+    Match(
+        status: "Fixture",
+        players: [
+          Player(name: "player1", voites: 11),
+          Player(name: "player2", voites: 1),
+          Player(name: "player3", voites: 12),
+          Player(name: "player4", voites: 1),
+        ],
+        name: "Milan - SPAL",
+        dateBeginning: "11/14/2019 - 16:49",
+        dateEnd: "11/22/2019 - 16:49"),
+    Match(
+        status: "Fixture",
+        players: [
+          Player(name: "player1", voites: 11),
+          Player(name: "player2", voites: 1),
+          Player(name: "player3", voites: 12),
+          Player(name: "player4", voites: 1),
+        ],
+        name: "Milan - SPAL",
+        dateBeginning: "11/14/2019 - 16:49",
+        dateEnd: "11/22/2019 - 16:49"),
+    Match(
+        status: "Fixture",
+        players: [
+          Player(name: "player1", voites: 11),
+          Player(name: "player2", voites: 1),
+          Player(name: "player3", voites: 12),
+          Player(name: "player4", voites: 1),
+        ],
+        name: "Milan - SPAL",
+        dateBeginning: "11/14/2019 - 16:49",
+        dateEnd: "11/22/2019 - 16:49"),
+    Match(
+        status: "Fixture",
+        players: [
+          Player(name: "player1", voites: 11),
+          Player(name: "player2", voites: 1),
+          Player(name: "player3", voites: 12),
+          Player(name: "player4", voites: 1),
+        ],
+        name: "Milan - SPAL",
+        dateBeginning: "11/14/2019 - 16:49",
+        dateEnd: "11/22/2019 - 16:49"),
+    Match(
+        status: "Fixture",
+        players: [
+          Player(name: "player1", voites: 11),
+          Player(name: "player2", voites: 1),
+          Player(name: "player3", voites: 12),
+          Player(name: "player4", voites: 1),
+        ],
+        name: "Milan - SPAL",
+        dateBeginning: "11/14/2019 - 16:49",
+        dateEnd: "11/22/2019 - 16:49"),
+    Match(
+        status: "Fixture",
+        players: [
+          Player(name: "player1", voites: 11),
+          Player(name: "player2", voites: 1),
+          Player(name: "player3", voites: 12),
+          Player(name: "player4", voites: 1),
+        ],
+        name: "Milan - SPAL",
+        dateBeginning: "11/14/2019 - 16:49",
+        dateEnd: "11/22/2019 - 16:49"),
+    Match(
+        status: "Fixture",
+        players: [
+          Player(name: "player1", voites: 11),
+          Player(name: "player2", voites: 1),
+          Player(name: "player3", voites: 12),
+          Player(name: "player4", voites: 1),
+        ],
+        name: "Milan - SPAL",
+        dateBeginning: "11/14/2019 - 16:49",
+        dateEnd: "11/22/2019 - 16:49"),
+    Match(
+        status: "Fixture",
+        players: [
+          Player(name: "player1", voites: 11),
+          Player(name: "player2", voites: 1),
+          Player(name: "player3", voites: 12),
+          Player(name: "player4", voites: 1),
+        ],
+        name: "Milan - SPAL",
+        dateBeginning: "11/14/2019 - 16:49",
+        dateEnd: "11/22/2019 - 16:49"),
+    Match(
+        status: "Fixture",
+        players: [
+          Player(name: "player1", voites: 11),
+          Player(name: "player2", voites: 1),
+          Player(name: "player3", voites: 12),
+          Player(name: "player4", voites: 1),
+        ],
+        name: "Milan - SPAL",
+        dateBeginning: "11/14/2019 - 16:49",
+        dateEnd: "11/22/2019 - 16:49"),
+    Match(
+        status: "Fixture",
+        players: [
+          Player(name: "player1", voites: 11),
+          Player(name: "player2", voites: 1),
+          Player(name: "player3", voites: 12),
+          Player(name: "player4", voites: 1),
+        ],
+        name: "Milan - SPAL",
+        dateBeginning: "11/14/2019 - 16:49",
+        dateEnd: "11/22/2019 - 16:49"),
+    Match(
+        status: "Fixture",
+        players: [
+          Player(name: "player1", voites: 11),
+          Player(name: "player2", voites: 1),
+          Player(name: "player3", voites: 12),
+          Player(name: "player4", voites: 1),
+        ],
+        name: "Milan - SPAL",
+        dateBeginning: "11/14/2019 - 16:49",
+        dateEnd: "11/22/2019 - 16:49"),
+  ];
+void _neverSatisfied(context) async {
+  return showDialog<void>(
+    context: context,
+    barrierDismissible: false, // user must tap button!
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text('Rewind and remember'),
+        content: SingleChildScrollView(
+          child: ListBody(
+            children: <Widget>[
+              Text('You will never be satisfied.'),
+              Text('You\’re like me. I’m never satisfied.'),
+            ],
+          ),
+        ),
+        actions: <Widget>[
+          FlatButton(
+            child: Text('Regret'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      );
+    },
+  );
+}
+  // ListTile(
+  //     title: Text(m.name),
+  //     subtitle: Text("status: "+m.status),
+  //     trailing: Text("Begin "+m.dateBeginning+" \n\nEnd   "+m.dateEnd),
+  //     leading: CircleAvatar(child: Text("2"),),
+  //   )
+  List<Widget> build(BuildContext context) {
+    return matchs
+        .map((Match m) => Padding(
+              padding:
+                  const EdgeInsets.only(bottom: 5.0, left: 12.0, right: 12.0),
+              child: new Card(
+                  child: Row(
+                children: <Widget>[
+                  new Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            left: 10.0, top: 10.0, bottom: 8.0),
+                        child: new Align(
+                          alignment: Alignment.topLeft,
+                          child: new Padding(
+                            padding:
+                                const EdgeInsets.only(top: 10.0, bottom: 8.0),
+                            child: Row(
+                              children: <Widget>[
+                                Text("Milan - SPAL",
+                                    style: new TextStyle(
+                                        color: new Color.fromARGB(
+                                            255, 117, 117, 117),
+                                        fontSize: 18.0,
+                                        fontWeight: FontWeight.bold)),
+                                Text(" Fixture",
+                                    style: new TextStyle(
+                                        color: new Color.fromARGB(
+                                            255, 117, 117, 117),
+                                        fontSize: 11.0,
+                                        fontWeight: FontWeight.bold))
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            left: 10.0, top: 10.0, bottom: 8.0),
+                        child: Column(
+                          children: <Widget>[
+                            Text("11/23/2019 - 22:50",
+                                style: new TextStyle(
+                                    color:
+                                        new Color.fromARGB(255, 117, 117, 117),
+                                    fontSize: 11.0,
+                                    fontWeight: FontWeight.bold)),
+                            Text(" - 11/24/2019 - 00:50",
+                                style: new TextStyle(
+                                    color:
+                                        new Color.fromARGB(255, 117, 117, 117),
+                                    fontSize: 11.0,
+                                    fontWeight: FontWeight.bold))
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: ButtonBar(
+                      children: <Widget>[
+                        Material(
+                          color: Colors.white,
+                          child: Center(
+                            child: Ink(
+                              decoration: const ShapeDecoration(
+                                color: Colors.lightBlue,
+                                shape: CircleBorder(),
+                              ),
+                              child: IconButton(
+                                icon: Icon(Icons.edit),
+                                color: Colors.white,
+                                onPressed: () {
+                                  print("asd");
+                                  
+                                },
+                              ),
+                            ),
+                          ),
+                        ),
+                        Material(
+                          color: Colors.white,
+                          child: Center(
+                            child: Ink(
+                              decoration: const ShapeDecoration(
+                                color: Colors.lightBlue,
+                                shape: CircleBorder(),
+                              ),
+                              child: IconButton(
+                                icon: Icon(Icons.insert_chart),
+                                color: Colors.white,
+                                onPressed: () {
+                                  //  _neverSatisfied(context);
+                                },
+                              ),
+                            ),
+                          ),
+                        ),
+                        Material(
+                          color: Colors.white,
+                          child: Center(
+                            child: Ink(
+                              decoration: const ShapeDecoration(
+                                color: Colors.lightBlue,
+                                shape: CircleBorder(),
+                              ),
+                              child: IconButton(
+                                icon: Icon(Icons.delete),
+                                color: Colors.white,
+                                onPressed: () {},
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              )),
+            ))
+        .toList();
+  }
+}
