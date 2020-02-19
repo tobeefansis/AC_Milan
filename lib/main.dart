72634c33b5f0faf47b3bc4fcfc2c1aa98df08d5e
tobeefansis <40244173+tobeefansis@users.noreply.github.com>
2020-02-19 15:19:40 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,16 @@
-// Flutter code sample for Form
-
-// This example shows a [Form] with one [TextFormField] to enter an email
-// address and a [RaisedButton] to submit the form. A [GlobalKey] is used here
-// to identify the [Form] and validate input.
-//
-// ![](https://flutter.github.io/assets-for-api-docs/assets/widgets/form.png)
 
 import 'package:flutter/material.dart';
-import 'MatchList.dart';
+import 'package:my_test_projeasct/Login.dart';
 
 void main() => runApp(MyApp());
 
-
-/// This Widget is the main application widget.
 class MyApp extends StatelessWidget {
-  static const String _title = 'Flutter Code Sample';
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: _title,
+      title: 'Welcome to рFlutter',
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(title: const Text(_title)),
-        body: ListView(
-          children: MatchList().build(context),
-        ),
-      ),
+      home: Login(),
     );
   }
 }
