bb665865891ce24d4aa8b0ea5073f74e6c53409e
tobeefansis <40244173+tobeefansis@users.noreply.github.com>
2020-02-20 17:10:31 +0300

    Update Action.dart

--- lib/Action.dart
+++ lib/Action.dart
@@ -1,4 +1,4 @@
-
+import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart';
 
 void loginAsync(String loginStr, String passStr) async {
@@ -11,3 +11,22 @@ void loginAsync(String loginStr, String passStr) async {
   String body = response.body;
   print(body);
 }
+
+Route createRoute(Widget page) {
+  return PageRouteBuilder(
+    pageBuilder: (context, animation, secondaryAnimation) => page,
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      var begin = Offset(1.0, 0.0);
+      var end = Offset.zero;
+      var curve = Curves.ease;
+
+      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+      return SlideTransition(
+        position: animation.drive(tween),
+        child: child,
+      );
+    },
+  );
+}
+
