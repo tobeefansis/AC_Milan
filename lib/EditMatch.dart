4b6e5e664b396fcb49815e5ddb446b17f62bf74b
tobeefansis <40244173+tobeefansis@users.noreply.github.com>
2020-02-20 17:11:35 +0300

    Update EditMatch.dart

--- lib/EditMatch.dart
+++ lib/EditMatch.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:my_test_projeasct/PlayerCheckList.dart';
 
-import 'Config.dart';
-import 'DropDown.dart';
+import 'Action.dart';
+import 'DatePicker.dart';
+import 'SelectPlayer.dart';
 
 class EditMatch extends StatefulWidget {
   EditMatch({Key key}) : super(key: key);
@@ -14,12 +16,82 @@ class EditMatch extends StatefulWidget {
 class _EditMatchState extends State<EditMatch> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-       darkTheme: Config.getTheme(),
-       home:Scaffold(
-         appBar: AppBar(title: Text("test"),),
-         body: DropDown(),
-       )
+    return Scaffold(
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButton: FloatingActionButton.extended(
+        isExtended: true,
+        icon: Icon(Icons.navigate_next),
+        label: Text('Next'),
+        backgroundColor: Colors.blue[600],
+        onPressed: () {
+          Navigator.of(context).push(createRoute(SelecctPlayer()));
+
+          // Add your onPressed code here!
+        },
+      ),
+      appBar: AppBar(
+        title: Text("Edit"),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+          child: Card(
+            child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
+              child: Column(
+                children: <Widget>[
+                  Text(
+                    "Start",
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                      bottom: 10.0,
+                    ),
+                    child: DateTimePicker("asd", "asd"),
+                  ),
+                  Text(
+                    "End",
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                      bottom: 10.0,
+                    ),
+                    child: DateTimePicker("asd", "asd"),
+                  ),
+                  //  Container(child:  ListView(
+                  //     // children: <Widget>[
+                  //     //   Row(
+                  //     //     children: <Widget>[
+                  //     //       Transform.scale(
+                  //     //         scale: 1.4,
+                  //     //         child: Checkbox(
+                  //     //           activeColor: Colors.blue[600],
+                  //     //           onChanged: (bool t) {},
+                  //     //           value: false,
+                  //     //         ),
+                  //     //       ),
+                  //     //       Expanded(
+                  //     //         child: Text(
+                  //     //           "p",
+                  //     //           style: TextStyle(
+                  //     //               fontWeight: FontWeight.bold, fontSize: 18.0),
+                  //     //         ),
+                  //     //       )
+                  //     //     ],
+                  //     //   ),
+                  //     // ],
+                  //   ),)
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
