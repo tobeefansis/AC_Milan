e857432d05164e532f77efab5a04cc8fec7d261e
tobeefansis <40244173+tobeefansis@users.noreply.github.com>
2020-02-20 17:11:15 +0300

    Update Config.dart

--- lib/Config.dart
+++ lib/Config.dart
@@ -1,16 +1,12 @@
 import 'package:flutter/material.dart';
+
 class Config {
- static bool dark = false;
-  static bool isDark(){
+  static bool dark;
+  static bool isDark() {
     return dark;
   }
-  static ThemeData getTheme(){
-  
-  if (dark) {
-    return ThemeData.dark();
-  } else {
-    return ThemeData.light();
-  }
-    
+
+  static ThemeMode getTheme() {
+    return ThemeMode.system;
   }
 }
