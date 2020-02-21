dfdf9115291ded90b0c1bc48bceb4a0b299bcb05
tobeefansis <40244173+tobeefansis@users.noreply.github.com>
2020-02-21 16:01:44 +0300

    Update Match.dart

--- lib/Match.dart
+++ lib/Match.dart
@@ -1,6 +1,47 @@
 import 'Players.dart';
 
 class Match {
+  static List<Match> getMatch() {
+    return [
+      Match(
+        name: "name 1",
+        dateBeginning: "1/1/1",
+        dateEnd: "2/2/2",
+        status: "live",
+      ),
+      Match(
+        name: "name 2 sdfsdfsdfsdfsdfsddddddddddddddddd sdfsd fsdf",
+        dateBeginning: "1/1/1",
+        dateEnd: "2/2/2",
+        status: "live",
+      ),
+      Match(
+        name: "name 3",
+        dateBeginning: "1/1/1",
+        dateEnd: "2/2/2",
+        status: "live",
+      ),
+      Match(
+        name: "name 4",
+        dateBeginning: "1/1/1",
+        dateEnd: "2/2/2",
+        status: "live",
+      ),
+      Match(
+        name: "name 5",
+        dateBeginning: "1/1/1",
+        dateEnd: "2/2/2",
+        status: "live",
+      ),
+      Match(
+        name: "name 6",
+        dateBeginning: "1/1/1",
+        dateEnd: "2/2/2",
+        status: "live",
+      ),
+    ];
+  }
+
   String status;
   String name;
   String dateBeginning;
