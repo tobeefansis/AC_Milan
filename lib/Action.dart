e1be001b944d63f3775c68a5e8f3bb3b098930cb
tobeefansis <40244173+tobeefansis@users.noreply.github.com>
2020-02-25 16:56:10 +0300

    Update Action.dart

--- lib/Action.dart
+++ lib/Action.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart';
+import 'Match.dart';
 
-void loginAsync(String loginStr, String passStr) async {
+Future<String> loginAsync(String loginStr, String passStr) async {
   // set up POST request arguments
   String url = 'https://acmilan-api-integ.netcosports.com/api/acmilan/login';
   Map<String, String> headers = {"content-type": "application/json"};
@@ -9,7 +10,74 @@ void loginAsync(String loginStr, String passStr) async {
   // make POST request
   Response response = await post(url, headers: headers, body: json);
   String body = response.body;
-  print(body);
+  return body;
+}
+
+void deleteAsync(int id) async {
+  // set up POST request arguments
+  String url = 'https://acmilan-api-integ.netcosports.com/api/motm/votings/$id';
+  // make POST request
+  await delete(url, headers: {
+    "Content-Type": "application/json",
+    "Token": "58b2e429f19c592942a36072c44bdfab"
+  });
+}
+
+Future<String> getMatches() async {
+  // set up POST request arguments
+  String url = 'https://acmilan-api-integ.netcosports.com/api/motm/games';
+  Response response = await get(url, headers: {
+    "Content-Type": "application/json",
+    "Token": "58b2e429f19c592942a36072c44bdfab"
+  });
+  String body = response.body;
+  return body;
+}
+
+Future<String> getPlayers(String id) async {
+  // set up POST request arguments
+  String url =
+      'https://acmilan-api-integ.netcosports.com/api/motm/games/$id/players';
+  Response response = await get(url, headers: {
+    "Content-Type": "application/json",
+    "Token": "58b2e429f19c592942a36072c44bdfab"
+  });
+  String body = response.body;
+  return body;
+}
+
+void createMatch(String startDate,String endDate,String matchId,String players) async {
+  // set up POST request arguments
+  String url = 'https://acmilan-api-integ.netcosports.com/api/motm/votings';
+  String json =
+      "{\"start_date\":\"$startDate\",\"end_date\":\"$endDate\",\"match_id\":\"$matchId\",\"players\":$players}";
+  // make POST request
+  await post(url,
+      headers: {
+        "Content-Type": "application/json",
+        "Token": "58b2e429f19c592942a36072c44bdfab"
+      },
+      body: json);
+}
+
+void updateMatch(String id, String json) async {
+  // set up POST request arguments
+  String url = 'https://acmilan-api-integ.netcosports.com/api/motm/votings/$id';
+var t =   await put(url,
+      headers: {
+        "Content-Type": "application/json",
+        "Token": "58b2e429f19c592942a36072c44bdfab"
+      },
+      body: json);
+      print(t.body);
+}
+
+Future<List<Match>> getAsync() async {
+  Map<String, String> headers = {"Token": "58b2e429f19c592942a36072c44bdfab"};
+  var response = await get(
+      'https://acmilan-api-integ.netcosports.com/api/motm/votings',
+      headers: headers);
+  return Match.fromJsonToList(response.body);
 }
 
 Route createRoute(Widget page) {
@@ -29,4 +97,3 @@ Route createRoute(Widget page) {
     },
   );
 }
-
